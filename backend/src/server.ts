60948d7fd9f8aec7783aa36720353b07d202f9aa
Abhirami007 <psabhirami015@gmail.com>
2026-03-05 15:10:09 -0500

    Initial Backend setup

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Sync space API running");
+});
+
+const PORT = Number(process.env.PORT || 4000);
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
